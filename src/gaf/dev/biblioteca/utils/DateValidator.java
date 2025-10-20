ebe4f7d8e84ad4905a996bdeeb3610971707de93
Guilherme A. Fortes <guilhermefortesoffice@gmail.com>
2025-10-20 11:42:49 -0300

    Utils
    -Criação da interface para manipulação de datas, adaptável às necessidades das classes futuras

---
+++ src/gaf/dev/biblioteca/utils/DateValidator.java
@@ -0,0 +1,13 @@
+package gaf.dev.biblioteca.utils;
+
+public interface DateValidator
+{
+    boolean isValid(byte day, byte month, short year);
+
+    default boolean isLeapYear(short year)
+    {
+        if (year % 400 == 0) return true;
+        if (year % 4 == 0 && year % 100 != 0) return true;
+        return false;
+    }
+}
